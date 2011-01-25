07c26e48479505e6b4cb25f9b704c48893ee0265
mhsimonson <markhsimonson@hotmail.com>
2011-01-25 00:42:01 -0800

    Just commit, PLEASE GOD, take this message::

---
+++ thirty_minute_app/time/print_time_mhs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import time
+import datetime
+import cgi
+
+
+#print Content-type:text/html\r\n\r\n
+#print "Content-type:text/html"
+print '<html>'
+print '<head>'
+print ' ***** output from print_time_mhs.py *****'
+print '</head>'
+print '<p>'
+print '<body>'
+print 
+print '<h1> <strong> The current time is: <strong> </h1>'
+print
+print 
+print datetime.datetime.now()
+print
+print
+print '</body>'
+print '</html>'
+
+
+#print "Here is the time: %s" % time.time()
+#print "and again: %s" % datetime.datetime.now()
+
+
